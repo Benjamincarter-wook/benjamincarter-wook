5a6899f0f3d196bf9abcdd5aa2f2f6b5142dc48a
bcarter <benjamin.carter@unsw.edu.au>
2021-05-26 00:24:39 +1000

    boot swatch css and javascript attempt

---
+++ app.js
@@ -0,0 +1,23 @@
+let Strings = [
+    { text : "Ernie Ball 9's",
+        imgSrc :'images/EB-9.png'},
+    { text: "Ernie Ball 10's",   
+       imgSrc: 'images/EB-10.png'},
+    { text:"Ernie Ball 11's",
+       imgSrc: 'images/EB-11.png'},
+    {text: "Ernie Ball 12's",
+    imgSrc:'images/EB-12.png'}]
+
+document.querySelector('input[name=scale-length]:checked').value
+document.querySelector('input[name=tension]:checked').value
+
+let results = function(){
+    if('scale-length'=='gibson') && ('tension'=='loose') 
+    console.log('Strings[0]')
+}
+    else if($("scale-length"=="fender")) &&
+}
+document.getElementsByClassName('submit').addEventListener('click',() =>
+{
+ console.log('results') 
+})
