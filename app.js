5ff6283b9b4f1a2df6c3b58a0d0077230eebe813
bcarter <benjamin.carter@unsw.edu.au>
2021-05-26 11:48:50 +1000

    class project time
    JS and CSS work, added modal

--- app.js
+++ app.js
@@ -8,16 +8,51 @@ let Strings = [
     {text: "Ernie Ball 12's",
     imgSrc:'images/EB-12.png'}]
 
-document.querySelector('input[name=scale-length]:checked').value
-document.querySelector('input[name=tension]:checked').value
+// document.querySelector('input[name=scale-length]:checked').value
+// document.querySelector('input[name=tension]:checked').value
+
+// Get the modal
+let modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+let btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+// btn.onClick = function(){
+//   modal.style.display = "block";
+// }
+btn.addEventListener ("click",() => {
+    modal.style.display= "block"
+})
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function(){
+//   modal.style.display = "none";
+// }
+// span,addEventListener('click', ()=> {
+//     modal.style.display= "none"
+// })
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 let results = function(){
-    if('scale-length'=='gibson') && ('tension'=='loose') 
-    console.log('Strings[0]')
+    if('gibson'=='true' && 'loose'=='true') 
+    console.log('Strings[1]')
 }
-    else if($("scale-length"=="fender")) &&
-}
-document.getElementsByClassName('submit').addEventListener('click',() =>
-{
- console.log('results') 
-})
+//     else if($("scale-length"=="fender")) && ()
+// }
+// const submit = document.querySelector('#submit')
+// addEventListener('click',() =>{
+
+// }
+// {
+//  console.log('results') 
+// })
