71575c1876d56c8865df65d126c4d8939bfc0ed9
bcarter <benjamin.carter@unsw.edu.au>
2021-05-27 01:51:18 +1000

    options array working
    all combinations giving console results

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const form = document.querySelector('form')
 let Strings = [
     { text : "Ernie Ball 9's",
         imgSrc :'images/EB-9.png'},
@@ -7,52 +8,88 @@ let Strings = [
        imgSrc: 'images/EB-11.png'},
     {text: "Ernie Ball 12's",
     imgSrc:'images/EB-12.png'}]
-
-// document.querySelector('input[name=scale-length]:checked').value
-// document.querySelector('input[name=tension]:checked').value
+    
 
 // Get the modal
 let modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-let btn = document.getElementById('myBtn');
+let btn = document.getElementById('myBtn')
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
-// btn.onClick = function(){
-//   modal.style.display = "block";
-// }
-btn.addEventListener ("click",() => {
-    modal.style.display= "block"
-})
+btn.onclick = function(){
+    let gibson = document.getElementById('gibson');
+    let fender = document.getElementById('fender');
+    let loose = document.getElementById('btnradio3');
+    let medium = document.getElementById('btnradio2');
+    let tight = document.getElementById('btnradio1');
+    if (gibson.checked == true && loose.checked ==true){
+             console.log("test")}
+             
+    else if(fender.checked== true && loose.checked == true){    
+                console.log("test 2")}
+                else if(gibson.checked== true && medium.checked == true){    
+                    console.log("test 3")}
+                    else if(fender.checked== true && medium.checked == true){    
+                        console.log("test 4")}
+                        else if(gibson.checked== true && tight.checked == true){    
+                            console.log("test 5")}
+                            else if(fender.checked== true && tight.checked == true){    
+                                console.log("test 6")}
+                                
+   modal.style.display = "block";
+  
+}
+// btn.addEventListener ("click",() => {
+//     modal.style.display= "block";
+// })
 
 // When the user clicks on <span> (x), close the modal
-// span.onclick = function(){
-//   modal.style.display = "none";
-// }
+span.onclick = function(){
+  modal.style.display = "none";
+}
 // span,addEventListener('click', ()=> {
 //     modal.style.display= "none"
 // })
 
 // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
-let results = function(){
-    if('gibson'=='true' && 'loose'=='true') 
-    console.log('Strings[1]')
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
-//     else if($("scale-length"=="fender")) && ()
-// }
-// const submit = document.querySelector('#submit')
-// addEventListener('click',() =>{
 
-// }
-// {
-//  console.log('results') 
+
+// form.addEventListener('submit', function(event){
+
 // })
+
+function results(){
+    
+    }
+
+
+// document.getElementById("myBtn").addEventListener = ('submit', (event) => {
+//     let results = function(){
+//         if('scale'=='gibson' && 'tension'=='loose') ;
+//          console.log("test")
+//     }
+// }
+// )
+
+
+
+
+
+
+// //     else if($("scale-length"=="fender")) && ()
+// // }
+// // const submit = document.querySelector('#submit')
+// // addEventListener('click',() =>{
+
+// // }
+// // {
+// //  console.log('results') 
